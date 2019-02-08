8472cb1866ea190b8d3344e343804ca44d7103b0
Hristo Hristov <hristo_lv96@abv.bg>
2019-02-09 00:57:09 +0200

    Adds unit tests for InferenceEngine::concludeAnAnswer()

--- src/source/InferenceEngine_test.go
+++ src/source/InferenceEngine_test.go
@@ -33,12 +33,12 @@ func TestProcessResponse(t *testing.T) {
 		response      Response
 		expected      int
 	}{
-		{nameForMethod: "Get Best Guess Index Q0 Yes", input: 0, response: (Yes), expected: 0},
-		{nameForMethod: "Get Best Guess Index Q0 No", input: 0, response: (No), expected: 2},
-		{nameForMethod: "Get Best Guess Index Q0 DK", input: 0, response: (DontKnowOrIrrelevant), expected: 1},
-		{nameForMethod: "Get Best Guess Index Q1 Yes", input: 1, response: (Yes), expected: 0},
-		{nameForMethod: "Get Best Guess Index Q1 No", input: 1, response: (No), expected: 0},
-		{nameForMethod: "Get Best Guess Index Q1 DK", input: 1, response: (DontKnowOrIrrelevant), expected: 2},
+		{nameForMethod: "Get Best Guess Index Q0 Yes", input: 0, response: Yes, expected: 0},
+		{nameForMethod: "Get Best Guess Index Q0 No", input: 0, response: No, expected: 2},
+		{nameForMethod: "Get Best Guess Index Q0 DK", input: 0, response: DontKnowOrIrrelevant, expected: 1},
+		{nameForMethod: "Get Best Guess Index Q1 Yes", input: 1, response: Yes, expected: 0},
+		{nameForMethod: "Get Best Guess Index Q1 No", input: 1, response: No, expected: 0},
+		{nameForMethod: "Get Best Guess Index Q1 DK", input: 1, response: DontKnowOrIrrelevant, expected: 2},
 	}
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("TestProcessResponse(%d)(%s)", test.input, test.response.toString()), func(t *testing.T) {
@@ -102,6 +102,86 @@ func TestAskQuestion(t *testing.T) {
 		})
 	}
 }
+
+func TestConcludeAnAnswer(t *testing.T) {
+	records := []Record{
+		*RecordConstructor(
+			"one",
+			[]Statistic{
+				*StatisticConstructor([StatisticSize]int{13, 0, 0}),
+				*StatisticConstructor([StatisticSize]int{3, 35, 0}),
+				*StatisticConstructor([StatisticSize]int{35, 35, 2000}),
+				*StatisticConstructor([StatisticSize]int{35, 35, 0}),
+				*StatisticConstructor([StatisticSize]int{35, 35, 0}),
+			}),
+		*RecordConstructor(
+			"two",
+			[]Statistic{
+				*StatisticConstructor([StatisticSize]int{13, 1, 1}),
+				*StatisticConstructor([StatisticSize]int{2, 0, 0}),
+				*StatisticConstructor([StatisticSize]int{2, 2, 0}),
+				*StatisticConstructor([StatisticSize]int{2, 2, 0}),
+				*StatisticConstructor([StatisticSize]int{2, 2, 0}),
+			}),
+		*RecordConstructor(
+			"three",
+			[]Statistic{
+				*StatisticConstructor([StatisticSize]int{1, 3, 0}),
+				*StatisticConstructor([StatisticSize]int{4, 1, 35}),
+				*StatisticConstructor([StatisticSize]int{4, 1, 35}),
+				*StatisticConstructor([StatisticSize]int{3, 3, 0}),
+				*StatisticConstructor([StatisticSize]int{4, 4, 35}),
+			}),
+	}
+
+	type Input struct {
+		index    int
+		response Response
+	}
+
+	var tests = []struct {
+		nameForMethod string
+		input         []Input
+		expected      int
+	}{
+		{
+			nameForMethod: "Conclude the second answer",
+			input: []Input{
+				{0, DontKnowOrIrrelevant},
+				{1, Yes},
+				{2, No},
+				{3, No},
+				{4, No},
+			},
+			expected: 1,
+		},
+		{
+			nameForMethod: "Conclude the first answer",
+			input: []Input{
+				{0, Yes},
+				{1, No},
+				{2, DontKnowOrIrrelevant},
+				{3, Yes},
+				{4, Yes},
+			},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		ie := InferenceEngineConstructor(records, []string{}, DataBaseOfFactsConstructor(len(records[0].statistics)), &FakeRandomGenerator{})
+		for _, input := range test.input {
+			ie.processResponse(input.index, input.response)
+		}
+		ie.enquiriesSinceLastAnswer = 500
+		_, resultIndex := ie.concludeAnAnswer()
+		if resultIndex != test.expected {
+			createError(t, test.nameForMethod, resultIndex, test.expected)
+		}
+	}
+}
+
+// Helper stuff //
 
 func createError(t *testing.T, nameOfMethod string, returned, expected int) {
 	t.Error(nameOfMethod + " returned " + strconv.Itoa(returned) + " instead of " + strconv.Itoa(expected))
